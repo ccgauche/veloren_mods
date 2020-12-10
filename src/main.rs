38e5cd4588065572e54b758cec098ab87c6718e7
ccgauche <gaucheron.laurent@gmail.com>
2020-12-10 19:22:36 +0100

    Added simple benchmarking

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 
+use std::time::Instant;
+
 use api::{Plugin, PreparedEventQuery};
 use wasmer_runtime::*;
 
@@ -11,16 +13,18 @@ use veloren_api_common::events::*;
 
 fn main() {
 
+    let before = Instant::now();
     let plugin= Plugin::new("wasmplugin".to_owned(), &WASM);
+    println!("Plugin compiled by WASM in: {:#?}", before.elapsed());
 
+
+    let before = Instant::now();
     let prepared = PreparedEventQuery::new(&PlayerJoinEvent {
         player_name: "ccgauche".to_owned(),
         player_id: 1515166644
     }).expect("Can't build WASM request");
-
-    
-    println!("{:#?}", plugin.try_execute("on_player_join", &prepared));
-    println!("{:#?}", plugin.try_execute("on_player_join", &prepared));
-    println!("{:#?}", plugin.try_execute("on_player_join", &prepared));
-    println!("{:#?}", plugin.try_execute("on_player_join", &prepared));
+    println!("Request prepared in: {:#?}", before.elapsed());
+    let before = Instant::now();
+    plugin.try_execute("on_player_join", &prepared);
+    println!("WASM call made in {:#?}", before.elapsed());
 }
--- wasmplugin/src/lib.rs
+++ wasmplugin/src/lib.rs
@@ -4,12 +4,12 @@ use events::*;
 
 #[export_function]
 pub fn on_player_join(input: PlayerJoinEvent) -> PlayerJoinResult {
-    send_actions(vec![Action::PlayerSendMessage(input.player_id,format!("Welcome {} on our server",input.player_name))]);
-    if input.player_name == "Cheater123" {
+    //send_actions(vec![Action::PlayerSendMessage(input.player_id,format!("Welcome {} on our server",input.player_name))]);
+    /*if input.player_name == "Cheater123" {
         PlayerJoinResult {
             keep_connected: false
         }
-    } else {
+    } else {*/
         PlayerJoinResult::default()
-    }
+    //}
 }
