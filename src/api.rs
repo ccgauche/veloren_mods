fc23a99991410a67a6a7d2813ddc6204b9da752d
ccgauche <gaucheron.laurent@gmail.com>
2020-12-11 16:14:02 +0100

    Performances improved

--- src/api.rs
+++ src/api.rs
@@ -3,7 +3,7 @@ use std::{collections::HashMap, marker::PhantomData, sync::Arc};
 use serde::de::DeserializeOwned;
 use wasmer_runtime::*;
 
-use veloren_api_common::{Action,events};
+use veloren_api_common::{events, Action};
 
 pub struct Plugin {
     name: String,
@@ -12,20 +12,28 @@ pub struct Plugin {
 }
 
 impl Plugin {
-
     pub fn new(name: String, bytes: &[u8]) -> Self {
-        let instance = instantiate(&bytes, &imports!{"env" => {
-            "send_action" => func!(read_action),
-        }}).expect("failed to instantiate wasm module");
-        
+        let module = compile(&bytes).expect("Can't compile WASM");
+        let instance = module
+            .instantiate(&imports! {"env" => {
+                "send_action" => func!(read_action),
+            }})
+            .expect("Can't import globals and instanciate WASM");
         Self {
             name,
-            events: instance.exports.into_iter().map(|(name,_)| name).collect(),
+            events: instance.exports.into_iter().map(|(name, _)| name).collect(),
             wasm_instance: instance,
         }
     }
 
-    pub fn try_execute<T>(&self, event_name: &str, request: &PreparedEventQuery<T>) -> Option<T::Response> where T: events::Event {
+    pub fn try_execute<T>(
+        &self,
+        event_name: &str,
+        request: &PreparedEventQuery<T>,
+    ) -> Option<T::Response>
+    where
+        T: events::Event,
+    {
         flame::start("try execute");
         flame::start("check function names");
         if !self.events.iter().any(|x| x == event_name) {
@@ -39,7 +47,7 @@ impl Plugin {
         let mem = self.wasm_instance.context().memory(0);
         flame::end("get memory");
         flame::start("execute raw");
-        let bytes = execute_raw(&mem,&func, &request.bytes).ok()?;
+        let bytes = execute_raw(&mem, &func, &request.bytes).ok()?;
         flame::end("execute raw");
         flame::start("execute deserialize");
         let tmp = bincode::deserialize(&bytes).ok();
@@ -51,30 +59,44 @@ impl Plugin {
 
 pub struct PreparedEventQuery<T> {
     bytes: Vec<u8>,
-    _phantom: PhantomData<T>
+    _phantom: PhantomData<T>,
 }
 
-impl <T: events::Event> PreparedEventQuery<T> {
-    pub fn new(event: &T) -> Option<Self> where T: events::Event {
+impl<T: events::Event> PreparedEventQuery<T> {
+    pub fn new(event: &T) -> Option<Self>
+    where
+        T: events::Event,
+    {
         Some(Self {
             bytes: bincode::serialize(&event).ok()?,
-            _phantom: PhantomData::default()
+            _phantom: PhantomData::default(),
         })
     }
 }
 
-
-pub type Function<'a> = Func<'a,(i32, u32), i32>;
+pub type Function<'a> = Func<'a, (i32, u32), i32>;
 
 const MEMORY_POS: usize = 100000;
 
-pub fn execute<'a,T>(memory: &Memory, function: &Function,input: &impl serde::Serialize) -> Result<T, &'static str> where T: DeserializeOwned {
-    let bytes = bincode::serialize(&input).map_err(|_| "Failed to serialize structure with bincode")?;
+pub fn execute<'a, T>(
+    memory: &Memory,
+    function: &Function,
+    input: &impl serde::Serialize,
+) -> Result<T, &'static str>
+where
+    T: DeserializeOwned,
+{
+    let bytes =
+        bincode::serialize(&input).map_err(|_| "Failed to serialize structure with bincode")?;
     let updated_bytes = execute_raw(memory, function, &bytes)?;
     bincode::deserialize(&updated_bytes).map_err(|_| "Failed to convert wasm memory to output")
 }
 
-pub fn execute_raw(memory: &Memory, function: &Function,bytes: &[u8]) -> Result<Vec<u8>, &'static str> {
+pub fn execute_raw(
+    memory: &Memory,
+    function: &Function,
+    bytes: &[u8],
+) -> Result<Vec<u8>, &'static str> {
     flame::start("get memory view");
     let view = memory.view::<u8>();
     flame::end("get memory view");
@@ -85,29 +107,42 @@ pub fn execute_raw(memory: &Memory, function: &Function,bytes: &[u8]) -> Result<
     }
     flame::end("memory write");
     flame::start("call");
-    let start = function.call(MEMORY_POS as i32, len as u32).expect("Failed to execute function") as usize;
+    let start = function
+        .call(MEMORY_POS as i32, len as u32)
+        .expect("Failed to execute function") as usize;
     flame::end("call");
     flame::start("get new view");
-    let new_view = memory.view::<u8>();
+    let view = memory.view::<u8>();
     flame::end("get new view");
-    let mut new_len_bytes = [0u8;4];
+    let mut new_len_bytes = [0u8; 4];
     flame::start("get new view");
+    // It is probably better to dirrectly make the new_len_bytes
     for i in 0..4 {
-        new_len_bytes[i] = new_view.get(i + 1).map(|c| c.get()).unwrap_or(0);
+        // Since memory view is a more than 11500 elements array we can get the [1;4] without any bound checks
+        unsafe {
+            new_len_bytes[i] = view.get_unchecked(i + 1).get();
+        }
     }
     flame::end("get new view");
     let new_len = u32::from_ne_bytes(new_len_bytes) as usize;
     flame::start("view to data");
-    let tmp = new_view[start..start + new_len]
-    .iter()
-    .map(|c|c.get())
-    .collect();
+    let tmp = view[start..start + new_len]
+        .iter()
+        .map(|c| c.get())
+        .collect();
     flame::end("view to data");
     Ok(tmp)
 }
 
-pub fn execute_event<T>(memory: &Memory, function: &Function<'_>, event: &T) -> Result<T::Response, &'static str> where T: events::Event{
-    execute(&memory,&function,&event)
+pub fn execute_event<T>(
+    memory: &Memory,
+    function: &Function<'_>,
+    event: &T,
+) -> Result<T::Response, &'static str>
+where
+    T: events::Event,
+{
+    execute(&memory, &function, &event)
 }
 
 pub fn read_action(ctx: &mut Ctx, ptr: u32, len: u32) {
@@ -121,7 +156,7 @@ pub fn read_action(ctx: &mut Ctx, ptr: u32, len: u32) {
 
     let e: Option<Vec<Action>> = bincode::deserialize(&bytes).ok();
 
-    println!("Actions sended: {:?}",e);
+    println!("Actions sended: {:?}", e);
 
     // TODO: Handle actions
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-
 use std::{fs::File, time::Instant};
 
 use api::{Plugin, PreparedEventQuery};
@@ -6,31 +5,31 @@ use wasmer_runtime::*;
 
 mod api;
 
-
 //static WASM: &[u8] = include_bytes!("../wasmplugin/target/wasm32-unknown-unknown/release/wasmplugin.wasm");
 
 use veloren_api_common::events::*;
 
 fn main() {
-
-    let read = std::fs::read("./wasmplugin/target/wasm32-unknown-unknown/release/wasmplugin.wasm").expect("Can't find file");
+    let read = std::fs::read("./wasmplugin/target/wasm32-unknown-unknown/release/wasmplugin.wasm")
+        .expect("Can't find file");
 
     let before = Instant::now();
-    let plugin= Plugin::new("wasmplugin".to_owned(), &read);
+    let plugin = Plugin::new("wasmplugin".to_owned(), &read);
     println!("Plugin compiled by WASM in: {:#?}", before.elapsed());
-
 
     let before = Instant::now();
     let prepared = PreparedEventQuery::new(&PlayerJoinEvent {
         player_name: "ccgauche".to_owned(),
-        player_id: 1515166644
-    }).expect("Can't build WASM request");
+        player_id: 1515166644,
+    })
+    .expect("Can't build WASM request");
     println!("Request prepared in: {:#?}", before.elapsed());
-    for _ in 0..100 {
+    for _ in 0..5 {
         let before = Instant::now();
-        plugin.try_execute("on_player_join", &prepared);
+        let result = plugin.try_execute("on_player_join", &prepared);
         println!("WASM call made in {:#?}", before.elapsed());
+        println!("{:?}", result);
     }
-    
+
     flame::dump_html(&mut File::create("flame-graph.html").unwrap()).unwrap();
 }
